6f62f1858088a2de3cf68cab129681fe052b3cc8
amir101001 <mulikov2012@mail.ru>
2022-11-05 23:00:56 +0300

    1

--- src/main/java/start/StartUi.java
+++ src/main/java/start/StartUi.java
@@ -1,15 +1,58 @@
 package start;
 
 import model.Employee;
+import service.EmployeeService;
+
+import java.util.Scanner;
 
 public class StartUi {
 
+    private EmployeeService employeeService;
+
+
+
+
+
     public static void Main(String args) {
-        Employee employee = new Employee("Amir", "Russia");
+   EmployeeService employeeService = new EmployeeService();
 
-        System.out.println(employee.getCountry());
-        System.out.println(employee.getName());
+        Scanner scanner = new Scanner(System.in);
 
+        boolean isRunning = true;
 
+        while(isRunning){
+            StartUi.showMenu();
+            System.out.println("Enter value of menu");
+            int value = scanner.nextInt();
+            if(value == 1){
+                System.out.println("Enter name");
+                String name = scanner.next();
+
+                System.out.println("Enter country");
+                String country = scanner.next();
+
+                Employee employee = new Employee(name, country);
+                employeeService.save(employee);
+            }else if(value == 2){
+                System.out.println("Enter id");
+                long id = scanner.nextLong();
+                employeeService.delete(id);
+            }else if(value == 3){
+                for(Employee employee : employeeService.findAll()){
+                    System.out.println(employee);
+                }
+            }else if(value ==4){
+                isRunning = false;
+                System.out.println("Close program. ");
+            }
+        }
+
+    }
+
+    private static void showMenu(){
+        System.out.println("1. Save employee. ");
+        System.out.println("2. Delete employee. ");
+        System.out.println("3, Find all employee");
+        System.out.println("4. Exit. ");
     }
 }
--- src/test/java/repository/MemEmployeeStoreTest.java
+++ src/test/java/repository/MemEmployeeStoreTest.java
@@ -4,15 +4,13 @@ import model.Employee;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MemEmployeeStoreTest {
     @Test
     public void whenSave(){
-        MemEmployeeStore memEmployeeStore = new MemEmployeeStore();
+        EmployeeRepository employeeRepository = new EmployeeRepository();
         Employee employee = new Employee("Amir", "Russia");
-        memEmployeeStore.save(employee);
-        Employee result = memEmployeeStore.finById(employee.getId());
+        employeeRepository.save(employee);
+        Employee result = employeeRepository.finById(employee.getId());
         Assert.assertNotNull(result);
     }
 
